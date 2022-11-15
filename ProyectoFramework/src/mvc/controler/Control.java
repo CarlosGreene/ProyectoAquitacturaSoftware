132466409597461b30ed69e587d165c9aac32687
CarlosGreene <59940440+CarlosGreene@users.noreply.github.com>
2022-11-15 10:04:14 -0600

    Update Control

--- ProyectoFramework/src/mvc/controler/Control.java
+++ ProyectoFramework/src/mvc/controler/Control.java
@@ -2,11 +2,17 @@ package mvc.controler;
 
 import java.util.Hashtable;
 
+import javax.swing.JFrame;
+
 import mvc.model.Model;
 
 public abstract class Control{
     protected Hashtable<Integer, Model> models;
+    protected JFrame view;
 
     public abstract void addModels(int key, Model model);
     public abstract void UpdateData(int key);
+    public void setView(JFrame view){
+        this.view = view;
+    }
 }

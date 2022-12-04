fc94a108471dc82749896cc86fc23ea1596e366d
CarlosGreene <59940440+CarlosGreene@users.noreply.github.com>
2022-12-04 15:54:44 -0600

    Update

--- ProyectoFramework/src/main.java
+++ ProyectoFramework/src/main.java
@@ -12,16 +12,19 @@ import reflection.ReflectionFrameWork;
 
 public class main {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+    private static final Logger LOG = Log.getLogger(main.class);
 
-        private static final Looger LOG = Log.getLogger(main.class);
-        
+    public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {        
         ReflectionFrameWork reflectionFrameWork = new ReflectionFrameWork("ConfigMVC.json","Votos1");
         Votes votes = new Votes();
         ArrayList<Observer> observers = new ArrayList<Observer>();
-        observers.add(new PieChart());
-        observers.add(new BarChart());
-        observers.add(new table());
+        //observers.add(new PieChart());
+        //observers.add(new BarChart());
+        //observers.add(new table());
+
+        LOG.info(observers.add(new PieChart()));
+        LOG.info(observers.add(new BarChart()));
+        LOG.info(observers.add(new table()));
 
         reflectionFrameWork.Execute(votes, observers);
 
